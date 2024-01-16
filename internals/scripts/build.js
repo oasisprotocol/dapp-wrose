eb42d4f2f01e3d9b9ef92086ca82ad19e984677c
Michal Zielenkiewicz <michal@oasisprotocol.org>
2024-01-16 16:07:56 +0100

    Clean up internal scripts

--- internals/scripts/build.js
+++ internals/scripts/build.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
+// @ts-check
 const execSync = require('child_process').execSync
-const buildSha = require('child_process').execSync('git rev-parse HEAD').toString().trim()
+const buildSha = execSync('git rev-parse HEAD').toString().trim()
 const buildDatetime = Date.now().toString()
 
 execSync(`tsc && REACT_APP_BUILD_SHA="${buildSha}" REACT_APP_BUILD_DATETIME="${buildDatetime}" vite build`, {
