5a8a352a41a2df987a6ad6408d77a7f1f2fd1fc6
lubej <matej.lubej@oasisprotocol.org>
2024-02-12 20:29:28 +0100

    Add explicit license identifier and author of icons

--- src/components/icons/OpenInNewIcon.tsx
+++ src/components/icons/OpenInNewIcon.tsx
@@ -1,3 +1,7 @@
+/**
+ * Copyright 2024 https://github.com/material-icons/material-icons
+ * SPDX-License-Identifier: Apache-2.0
+ */
 import { FC } from 'react'
 
 export const OpenInNewIcon: FC = () => (
