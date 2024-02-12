2c881865bc307a925341456c96958bf8c433af04
lubej <matej.lubej@oasisprotocol.org>
2024-02-12 20:16:07 +0100

    Rename fixtures names

--- src/utils/__tests__/number.utils.spec.ts
+++ src/utils/__tests__/number.utils.spec.ts
@@ -8,23 +8,7 @@ const { shouldShowWrapFeeWarningModal } = NumberUtils
 const gasPrice = utils.parseUnits('100', 'gwei')
 const fee = gasPrice.mul(MAX_GAS_LIMIT)
 
-test('should skip modal when account balance is less than retained fee', () => {
-  const amount = utils.parseEther('0.02')
-  const accountBalanceAmount = utils.parseEther('0.02')
-  const show = shouldShowWrapFeeWarningModal({ fee, amount, accountBalanceAmount })
-
-  expect(show).toEqual(false)
-})
-
-test('should skip modal when amount is less than retained fee', () => {
-  const amount = utils.parseEther('0.02')
-  const accountBalanceAmount = utils.parseEther('10')
-  const show = shouldShowWrapFeeWarningModal({ fee, amount, accountBalanceAmount })
-
-  expect(show).toEqual(false)
-})
-
-test('should show modal when amount plus retained fee is greater than account balance', () => {
+test('should show modal if user can not afford future transactions', () => {
   const amount = utils.parseEther('9.99')
   const accountBalanceAmount = utils.parseEther('10')
   const show = shouldShowWrapFeeWarningModal({ fee, amount, accountBalanceAmount })
@@ -32,10 +16,26 @@ test('should show modal when amount plus retained fee is greater than account ba
   expect(show).toEqual(true)
 })
 
-test('should skip modal when amount plus retained fee is less than account balance', () => {
+test('should skip modal if user can not afford to retain fees for future transactions', () => {
+  const amount = utils.parseEther('0.02')
+  const accountBalanceAmount = utils.parseEther('0.02')
+  const show = shouldShowWrapFeeWarningModal({ fee, amount, accountBalanceAmount })
+
+  expect(show).toEqual(false)
+})
+
+test('should skip modal if user can already afford future transactions', () => {
   const amount = utils.parseEther('20')
   const accountBalanceAmount = utils.parseEther('40')
 
+  const show = shouldShowWrapFeeWarningModal({ fee, amount, accountBalanceAmount })
+
+  expect(show).toEqual(false)
+})
+
+test('should skip modal if wrapping tiny amounts', () => {
+  const amount = utils.parseEther('0.02')
+  const accountBalanceAmount = utils.parseEther('10')
   const show = shouldShowWrapFeeWarningModal({ fee, amount, accountBalanceAmount })
 
   expect(show).toEqual(false)
